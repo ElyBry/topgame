b69c6ecbca54575d8432489047e483e8ec26c9ce
Denis Vakulenko <101655241+woofmgn@users.noreply.github.com>
2025-01-31 13:36:38 +0500

    feat: add react-router and pages

---
+++ packages/client/src/utils/router.tsx
@@ -0,0 +1,68 @@
+import { redirect, RouteObject } from 'react-router-dom'
+import {
+  ForumPage,
+  GamePage,
+  LeaderboardPage,
+  MainPage,
+  NotFoundPage,
+  ProfilePage,
+  SigninPage,
+  SignupPage,
+  TopicPage,
+} from '../pages'
+import { ROUTES } from './routes'
+import { OpenLayout } from '../layouts/OpenLayout'
+import { PrivateLayout } from '../layouts/PrivateLayout'
+
+export const router: RouteObject[] = [
+  {
+    element: <OpenLayout />,
+    children: [
+      {
+        element: <SigninPage />,
+        path: ROUTES.SIGN_IN,
+      },
+      {
+        element: <SignupPage />,
+        path: ROUTES.SIGN_UP,
+      },
+      {
+        element: <NotFoundPage />,
+        path: ROUTES.NOT_FOUND,
+      },
+    ],
+  },
+  {
+    element: <PrivateLayout />,
+    children: [
+      {
+        element: <MainPage />,
+        path: ROUTES.MAIN,
+      },
+      {
+        element: <ProfilePage />,
+        path: ROUTES.PROFILE,
+      },
+      {
+        element: <GamePage />,
+        path: ROUTES.GAME,
+      },
+      {
+        element: <LeaderboardPage />,
+        path: ROUTES.LEADERBOARD,
+      },
+      {
+        element: <ForumPage />,
+        path: ROUTES.FORUM,
+      },
+      {
+        element: <TopicPage />,
+        path: ROUTES.FORUM_MESSAGE,
+      },
+    ],
+  },
+  {
+    path: '*',
+    loader: () => redirect(ROUTES.NOT_FOUND),
+  },
+]
