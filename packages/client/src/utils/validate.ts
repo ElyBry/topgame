114542619e75dbc68e920531b669629898ca8b73
MJulia111 <julia.m241992@gmail.com>
2025-02-04 14:34:59 +0200

    feat: add validate rules

---
+++ packages/client/src/utils/validate.ts
@@ -0,0 +1,34 @@
+import {
+  checkEmail,
+  checkLogin,
+  checkPhone,
+  checkPassword,
+  checkFirstAndLastNames,
+} from "./rules.ts";
+
+export const validateField = (key: string, value: string) => {
+  let error = "";
+
+  switch (key) {
+    case "email":
+      error = checkEmail(value);
+      break;
+    case "login":
+      error = checkLogin(value);
+      break;
+    case "phone":
+      error = checkPhone(value);
+      break;
+    case "password":
+      error = checkPassword(value);
+      break;
+    case "first_name":
+      error = checkFirstAndLastNames(value, "Имя");
+      break;
+    case "second_name":
+      error = checkFirstAndLastNames(value, "Фамилия");
+      break;
+  }
+
+  return error;
+};
