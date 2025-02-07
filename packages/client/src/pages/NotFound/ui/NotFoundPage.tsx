3c6998657019bbd43797c46593d4fc14f1add761
MJulia111 <julia.m241992@gmail.com>
2025-02-07 12:32:14 +0200

    feat: Создание страницы not found

--- packages/client/src/pages/NotFound/ui/NotFoundPage.tsx
+++ packages/client/src/pages/NotFound/ui/NotFoundPage.tsx
@@ -1,3 +1,26 @@
+import stylesButton from '../../../components/Button/Button.module.css'
+import Button from '../../../components/Button/Button'
+import styles from './NotFoundPage.module.css'
+import { useNavigate } from 'react-router-dom'
+import { ROUTES } from '../../../utils/routes'
+
 export const NotFoundPage = () => {
-  return <div>Такой страницы не существует</div>
+  const navigate = useNavigate()
+
+		const handleNavigateToMain = () => {
+			navigate(ROUTES.MAIN)
+		}
+
+  return (
+    <main className={styles.not_found}>
+      <h1 className={styles.not_found_number}>400</h1>
+      <p className={styles.not_found_text}>Не туда попали</p>
+			<Button
+            label="На главную страницу"
+            className={stylesButton.button_link}
+            type="button"
+            onClick={handleNavigateToMain}
+          />
+    </main>
+  )
 }
