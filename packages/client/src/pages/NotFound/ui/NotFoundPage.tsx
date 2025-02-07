75617ede927695b2fa15d3be1630b22cfad06791
MJulia111 <julia.m241992@gmail.com>
2025-02-07 12:44:12 +0200

    fix: Пофигшено форматирвоание

--- packages/client/src/pages/NotFound/ui/NotFoundPage.tsx
+++ packages/client/src/pages/NotFound/ui/NotFoundPage.tsx
@@ -7,20 +7,20 @@ import { ROUTES } from '../../../utils/routes'
 export const NotFoundPage = () => {
   const navigate = useNavigate()
 
-		const handleNavigateToMain = () => {
-			navigate(ROUTES.MAIN)
-		}
+  const handleNavigateToMain = () => {
+    navigate(ROUTES.MAIN)
+  }
 
   return (
     <main className={styles.not_found}>
       <h1 className={styles.not_found_number}>400</h1>
       <p className={styles.not_found_text}>Не туда попали</p>
-			<Button
-            label="На главную страницу"
-            className={stylesButton.button_link}
-            type="button"
-            onClick={handleNavigateToMain}
-          />
+      <Button
+        label="На главную страницу"
+        className={stylesButton.button_link}
+        type="button"
+        onClick={handleNavigateToMain}
+      />
     </main>
   )
 }
