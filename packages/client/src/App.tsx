3893653ead7be6f525882c9a859a7cd784423215
Denis Vakulenko <101655241+woofmgn@users.noreply.github.com>
2025-02-17 13:59:32 +0500

    feat: add redux and user slice

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
 import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { router } from './utils/router'
-import { UserContextProvider } from './context/userContext'
-import { TUserInfoResponse } from './api/auth/userInfoApi'
 import { AppLayout } from './layouts/AppLayout'
+import { Provider } from 'react-redux'
+import { store } from './store/config/store'
 
 function App() {
-  const [userData, setUserData] = useState<TUserInfoResponse | null>(null)
-  const [isLoading, setIsLoading] = useState(true)
-
   // const fetchServerData = useCallback(async () => {
   //   const url = `http://localhost:${__SERVER_PORT__}`
   //   const response = await fetch(url)
@@ -22,17 +18,11 @@ function App() {
   // }, [fetchServerData])
 
   return (
-    <UserContextProvider
-      value={{
-        userInfo: userData,
-        loading: isLoading,
-        setUserInfo: setUserData,
-        setLoading: setIsLoading,
-      }}>
+    <Provider store={store}>
       <AppLayout>
         <RouterProvider router={createBrowserRouter(router)} />
       </AppLayout>
-    </UserContextProvider>
+    </Provider>
   )
 }
 
--- packages/client/src/layouts/OpenLayout/ui/OpenLayout.tsx
+++ packages/client/src/layouts/OpenLayout/ui/OpenLayout.tsx
@@ -1,18 +1,18 @@
-import { useContext, useEffect } from 'react'
+import { useEffect } from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
-import UserContext from '../../../context/userContext'
 import { ROUTES } from '../../../utils/routes'
+import { useAppSelector } from '../../../store/hooks'
 
 export const OpenLayout = () => {
   const navigate = useNavigate()
-  const context = useContext(UserContext)
+
+  const { user } = useAppSelector(state => state.userSlice)
 
   useEffect(() => {
-    if (context?.userInfo) {
+    if (user) {
       navigate(ROUTES.MAIN)
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [navigate])
+  }, [user, navigate])
 
   return <Outlet />
 }
