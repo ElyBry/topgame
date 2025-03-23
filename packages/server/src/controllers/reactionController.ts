ddbdd1b5a9a1d72c4c875ffa6d22df089b4417b8
MJulia111 <julia.m241992@gmail.com>
2025-03-24 01:48:55 +0200

    new: Настройка sequelize, подключения к бд, создание апи для реакций к топикам

---
+++ packages/server/src/controllers/reactionController.ts
@@ -0,0 +1,53 @@
+import { sequelize } from '../db'
+import { reactionModel } from '../database/models/reaction'
+import { topicModel } from '../database/models/topic'
+import { userModel } from '../database/models/user'
+
+export const Topic = sequelize.define('topic', topicModel)
+export const Reaction = sequelize.define('reaction', reactionModel)
+export const User = sequelize.define('user', userModel)
+
+Topic.belongsTo(User, {
+  foreignKey: 'userId',
+  as: 'user',
+})
+Reaction.belongsTo(Topic, {
+  foreignKey: 'topicId',
+  as: 'topic',
+})
+Reaction.belongsTo(User, {
+  foreignKey: 'userId',
+  as: 'user',
+})
+
+export const addReaction = async (req: any, res: any) => {
+  try {
+    const { userId, topicId, type } = req.body
+    const reactions = await Reaction.create({
+      userId,
+      topicId,
+      type,
+    })
+
+    res.send(reactions)
+  } catch (err) {
+    console.error(err)
+    res.status(500).send('Unexpected error occurred on server!')
+  }
+}
+
+export const getReactions = async (req: any, res: any) => {
+  try {
+    const { topicId } = req.params
+    const reactions = await Reaction.findAll({
+      where: {
+        topicId: Number(topicId),
+      },
+    })
+
+    res.send(reactions)
+  } catch (err) {
+    console.error(err)
+    res.status(500).send('Unexpected error occurred on server!')
+  }
+}
