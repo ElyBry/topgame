bb856d33fb0cb8693f1df3dd874d32fea7af635c
yudin-1337 <yudin-1337@yandex.ru>
2025-03-25 19:55:35 +0500

    Реализовал API для форума TOPGAME-35

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -4,15 +4,18 @@ import express from 'express'
 import { dbConnect } from './db'
 
 import reactionRouter from './routes/reactionRouter'
+import topicRouter from './routes/topicRouter'
 
 dotenv.config()
 
 const app = express()
 app.use(cors())
+app.use(express.json())
 const port = Number(process.env.SERVER_PORT) || 3001
 
 dbConnect()
 
+app.use('/topics/', topicRouter)
 app.use('/topics/:topicId/', reactionRouter)
 
 app.use(express.static('./'))
