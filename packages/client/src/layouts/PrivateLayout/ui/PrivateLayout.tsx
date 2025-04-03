44c433d6128e152e1a18b426d6818b5c5efeb1ac
kt-kk8kk <kt+kk8kk@yakushevsky.com>
2025-04-03 22:57:25 +0200

    fix layout and db files

--- packages/client/src/layouts/PrivateLayout/ui/PrivateLayout.tsx
+++ packages/client/src/layouts/PrivateLayout/ui/PrivateLayout.tsx
@@ -1,17 +1,24 @@
 import { useEffect } from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
 import { ROUTES } from '../../../utils/routes'
-import { useAppSelector } from '../../../store/hooks'
+import { useAppSelector, useAppDispatch } from '../../../store/hooks'
+import { fetchUser } from '../../../store/slice/userSlice'
 
 export const PrivateLayout = () => {
   const navigate = useNavigate()
-
   const { user, status } = useAppSelector(state => state.userSlice)
+  const dispatch = useAppDispatch()
+  
 
   useEffect(() => {
     if (!user && status !== 'loading') {
       navigate(ROUTES.SIGN_IN)
     }
+
+    if (!user) {
+      dispatch(fetchUser())
+    }
+    
   }, [navigate, status, user])
 
   return <Outlet />
