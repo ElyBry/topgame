0973b40d613caf4e37856e7b579bb7f23cc681ff
kt-kk8kk <kt+kk8kk@yakushevsky.com>
2025-04-12 00:14:23 +0200

    add new user's registration

--- packages/client/src/api/auth/authApi.ts
+++ packages/client/src/api/auth/authApi.ts
@@ -6,6 +6,24 @@ type TLoginArgs = {
   password: string
 }
 
+type TSignupArgs = {
+  first_name: string
+  second_name: string
+  login: string
+  email: string
+  password: string
+  phone: string
+}
+
+export const signup = async (userData: TSignupArgs) => {
+  try {
+    const response = await axiosRequest.post(ENDPOINTS.SIGN_UP, userData)
+    return response
+  } catch (error: any) {
+    throw error.response?.data || { reason: 'Unknown error' }
+  }
+}
+
 export const login = async ({ login, password }: TLoginArgs) => {
   const response = await axiosRequest.post(ENDPOINTS.SIGN_IN, {
     login,
