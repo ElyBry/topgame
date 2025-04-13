a4158c9f7f1f8f7f72f2576d6a0f83e871cdab4d
kt-kk8kk <kt+kk8kk@yakushevsky.com>
2025-04-13 22:21:21 +0200

    add Devs descriptions and avatars

--- packages/client/src/components/Team/Team.tsx
+++ packages/client/src/components/Team/Team.tsx
@@ -1,21 +1,33 @@
 import styles from './Team.module.css';
 
 interface TeamProps {
-  pic: string;
+  pic?: string;
   h4: string;
   copy: string;
 }
 
 const Team: React.FC<TeamProps> = ({ pic, h4, copy }) => {
+  const initials = h4
+    .split(' ')
+    .map(word => word[0]?.toUpperCase())
+    .join('')
+    .slice(0, 2);
+
   return (
     <div className={styles.team}>
-      <div className={styles.pic}>{pic}</div>
+      <div className={styles.pic}>
+        {pic ? (
+          <img className={styles.img} src={`/devs/${pic}`} alt={h4} />
+        ) : (
+          <div className={styles.initials}>{initials}</div>
+        )}
+      </div>
       <div className={styles.holder}>
         <h4 className={styles.h4}>{h4}</h4>
         <p>{copy}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Team
+export default Team;
